872199df3404b2a2cb8a0daa6e0358a65ec3f4c4
Wbenaraiba <Wbenaraiba@dept-info.crosemont.quebec>
2023-12-19 20:37:12 -0500

    quelques changements des configurations

--- StageAvenirAPI/build.gradle.kts
+++ StageAvenirAPI/build.gradle.kts
@@ -19,12 +19,18 @@ repositories {
 }
 
 dependencies {
+
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security:spring-security-oauth2-resource-server")
+    implementation("org.springframework.security:spring-security-oauth2-jose")
+    implementation("org.springframework.security:spring-security-config")
+
 }
 
 tasks.withType<KotlinCompile> {
